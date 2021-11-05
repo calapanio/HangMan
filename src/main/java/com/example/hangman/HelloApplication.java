860d00de06d19ad3fc30ced696c7f7e2118302f0
caleb <caleb.perry@cecstudents.org>
2021-11-04 19:41:48 -0600

    start

--- src/main/java/com/example/hangman/HelloApplication.java
+++ src/main/java/com/example/hangman/HelloApplication.java
@@ -1,39 +1,30 @@
 package com.example.hangman;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.canvas.*;
-import javafx.scene.web.*;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class HelloApplication extends Application {
     String letter;
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage){
         stage.setTitle("Hangman");
-        Label word = new Label("Hangman");
-        word.relocate(550, 10);
+        Label title = new Label("Hangman");
+        title.relocate(550, 10);
         Font wordFont = new Font(200);
-        word.setFont(wordFont);
+        title.setFont(wordFont);
 
-        Pane pane = new Pane(word);
+        Pane pane = new Pane(title);
         Scene scene = new Scene(pane, 300 ,400);
         stage.setScene(scene);
         stage.show();
-        
+
+        ArrayList<Character> word = splitWord(getWord());
     }
     public static ArrayList<Character> splitWord(String word){
         ArrayList<Character> value = new ArrayList<>();
@@ -42,6 +33,7 @@ public class HelloApplication extends Application {
         }
         return value;
     }
+    public static String getWord(){return "";}
 
     public static void main(String[] args) {
         launch();
