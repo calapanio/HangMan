f1e9c97d571d0bac7a05c848a680c772bd4b326d
caleb <caleb.perry@cecstudents.org>
2021-11-06 12:01:11 -0600

    added words and a word spliter

--- src/main/java/com/example/hangman/HelloApplication.java
+++ src/main/java/com/example/hangman/HelloApplication.java
@@ -2,29 +2,36 @@ package com.example.hangman;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class HelloApplication extends Application {
     String letter;
     @Override
     public void start(Stage stage){
+        ArrayList<Character> hiddenWord = splitWord(getWord());
         stage.setTitle("Hangman");
         Label title = new Label("Hangman");
         title.relocate(550, 10);
         Font wordFont = new Font(200);
         title.setFont(wordFont);
 
-        Pane pane = new Pane(title);
+        Text check = new Text();
+        check.setText(String.valueOf(hiddenWord.get(1)));
+        HBox hBox1 = new HBox(check);
+        VBox vBox1 = new VBox(hBox1);
+
+        Pane pane = new Pane(title, vBox1);
         Scene scene = new Scene(pane, 300 ,400);
         stage.setScene(scene);
         stage.show();
-
-        ArrayList<Character> word = splitWord(getWord());
     }
     public static ArrayList<Character> splitWord(String word){
         ArrayList<Character> value = new ArrayList<>();
@@ -33,7 +40,18 @@ public class HelloApplication extends Application {
         }
         return value;
     }
-    public static String getWord(){return "";}
+    public static String getWord(){
+        String[] words = {"jacket", "panic", "shortage", "peel",
+            "compliance", "branch", "good", "vain", "strict", "mountain", "pace",
+            "umbrella", "ethics", "mass", "tiptoe", "despair", "prefer", "establish",
+            "peanut", "quest", "jealous", "bell", "disagreement", "swear", "release",
+            "respect", "section", "predict", "harvest", "production", "mobile", "my",
+            "care", "brother", "helpless", "disaster", "grounds", "code", "gossip",
+            "marsh", "survival", "lion", "storm", "revoke", "sniff", "reverse",
+            "bracket", "decay", "boom", "generate"};
+        Random random = new Random();
+        return words[random.nextInt(words.length)];
+    }
 
     public static void main(String[] args) {
         launch();
